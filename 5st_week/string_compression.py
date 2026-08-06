a226ece2604b7280bd68f84bc39a1f469c8d9fcc
CHOI DAE GEON <cdg@aplayz.co.kr>
2026-08-07 07:50:45 +0900

    get correct parentheses 진행

--- 5st_week/string_compression.py
+++ 5st_week/string_compression.py
@@ -1,44 +1,37 @@
 input = "abcabcabcabcdededededede"
 
-# aabbaccc
-# a a b b a c c c => 2a2ba3c
 
-# ababcdcdababcdcd
-# ab ab cd cd ab ab cd cd => 2ab2cd2ab2cd
-# ababcdcd ababcdcd => 2ababcdcd
+def string_compression(string):
+    n = len(string)
+    result = n
+    for split_size in range(1,n//2+1):
+        splited = [
+            string[i:i+split_size] for i in range(0,n,split_size)
+        ]
+        compressed = ""
+        count = 1
+        for i in range(0,len(splited)-1):
+            cur, next = splited[i],splited[i+1]
 
-# abcabcdede
-# abc abc dede => 2abcdede
+            if cur == next:
+                count+=1
+            else:
+                if count == 1:
+                    compressed += cur
+                else:
+                    compressed += f"{count}{cur}"
+                count = 1
+        if count == 1:
+            compressed += splited[-1]
+        else:
+            compressed += f"{count}{splited[-1]}"
 
-# abcabcabcabcdededededede
-# abc abc abc abc ded ede ded ede => 4abcdededededede
-# abcabc abcabc dedede dedede => 2abcabc2dedede
-
-# xababcdcdababcdcd => 17
-
-# jaaa => j3a
-
-# AZAAAZDWAAA
-# A Z A A A Z D W A A A => AZ3AZDW3A
-
-# BBAABAAADABBBD
-# B B A A B A A A D A B B B D => 2B2AB3ADA3BD
-
-def string_compression(string =''):
-    N = len(string)//2
-    index = 0
-    while index<len(string):
-        print(string[index:index+3])
-        index+=3
-
-    # for i in range(1,N+1):
-    #     print(i)
-
-    return
+        result = min(len(compressed),result)
+    return result
 
 
 print(string_compression(input))  # 14 가 출력되어야 합니다!
 
-# print("정답 = 3 / 현재 풀이 값 = ", string_compression("JAAA"))
-# print("정답 = 9 / 현재 풀이 값 = ", string_compression("AZAAAZDWAAA"))
-# print("정답 = 12 / 현재 풀이 값 = ", string_compression('BBAABAAADABBBD'))
+print("정답 = 3 / 현재 풀이 값 = ", string_compression("JAAA"))
+print("정답 = 9 / 현재 풀이 값 = ", string_compression("AZAAAZDWAAA"))
+print("정답 = 12 / 현재 풀이 값 = ", string_compression('BBAABAAADABBBD'))
